8f270cdfe5821722b4b53545bd505103cc52f549
Pratyush Thota <pratyushthota@Pratyushs-MacBook-Pro.local>
2020-07-27 14:27:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <p><h1>Lets download an icon</h1></p>
+  
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ src/Components/alert.js
@@ -0,0 +1,6 @@
+function showAlert()
+{
+var myText= "Icon has been downloaded"
+alert(myText)
+}
+
