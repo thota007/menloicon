9d77082b4063b74539e76defba0c913c64a19571
Pratyush Thota <pratyushthota@Pratyushs-MacBook-Pro.local>
2020-07-30 12:17:52 +0530

    Updates

---
+++ precache-manifest.bde6b0a0083dd35c5605db2e9a87094b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c0e125416eafca419a7419b3043a969",
+    "url": "/menloicon/index.html"
+  },
+  {
+    "revision": "c8a0184fe73c049c7acc",
+    "url": "/menloicon/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "325e2447a4236c2a84df",
+    "url": "/menloicon/static/js/2.14a24939.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menloicon/static/js/2.14a24939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8a0184fe73c049c7acc",
+    "url": "/menloicon/static/js/main.9a73d2cb.chunk.js"
+  },
+  {
+    "revision": "7cd7fd7b1903c077f254",
+    "url": "/menloicon/static/js/runtime-main.664f7201.js"
+  }
+]);
